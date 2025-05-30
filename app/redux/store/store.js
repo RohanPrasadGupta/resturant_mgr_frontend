84a876cb72efc1aeb763ebdc2ee3bbaaba52f41e
ROHAN PRASAD GUPTA <90445636+RohanPrasadGupta@users.noreply.github.com>
2025-05-31 01:10:47 +0700

    update user login and redux

--- app/redux/store/store.js
+++ app/redux/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tableReducer from "../storeSlice/tableSlice";
+import loginUserSliceReducer from "../storeSlice/loginUserSlice";
 
 export default configureStore({
   reducer: {
-    selectedTable: tableReducer,
+    selectedUser: loginUserSliceReducer,
   },
 });
